f994014c2ad357c78e35f63a71fcb96dde36a9c0
adriano <adriano@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-11 11:48:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/commecol@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/print.treeNodf.R
+++ pkg/R/print.treeNodf.R
@@ -1,8 +1,12 @@
-print.treeNodf<- function (x, digits=4, ...){
-cat("treeNODF rows   :", format(x$treeNODF.rows, ...), "\n")
-cat("treeNODF columns:", format(x$treeNODF.cols, ...), "\n")
-cat("treeNODF matrix :", format(x$treeNODF.mat, ...) , "\n")
+print.treeNodf<- function (x, digits=3, ...){
+resu<-matrix(NA, 3, 3)
+rownames(resu) <- c("rows", "columns", "matrix")
+colnames(resu) <- c("treeNODF", "S.fraction", "topoNODF")
+resu[1,] <- x$rows
+resu[2,] <- x$cols
+resu[3,] <- x$mat
 
+print(resu)
 invisible(x)
 }
 
--- pkg/R/treeNodf.R
+++ pkg/R/treeNodf.R
@@ -1,46 +1,74 @@
 treeNodf<-function (comm, col.tree, order.rows=FALSE, row.tree, order.cols=FALSE) {
-
-comm <- ifelse(comm>0,1,0)
+comm <- ifelse(comm > 0, 1, 0)
 comm <- as.matrix(comm)
-resu<-list(
-         treeNODF.rows=NA,
-         treeNODF.cols=NA,
-         treeNODF.mat=NA,
-         treeNODF.rows.pairs=NA,
-         treeNODF.cols.pairs=NA        
+rows <- rep(NA, 3)
+names(rows)<-c('tree.nodf', 's.fraction', 'topo.nodf')
+cols <- rows
+mat  <- rows
+
+resu <- list(
+         rows = rows,
+         cols = cols,
+         mat  = mat,
+         
+         tree.nodf.rows.dist  = NA,
+         tree.nodf.cols.dist  = NA,
+         s.fraction.rows.dist = NA,
+         s.fraction.cols.dist = NA,
+         topo.nodf.rows.dist  = NA,
+         topo.nodf.cols.dist  = NA
          )
 
 # treeNodf to rows (sites).
 if(missing(col.tree)==FALSE){
-  if (is.null(col.tree$edge.length)) {stop("Tree has no branch lengths, cannot compute BL-diversity")}
-  if (!is.rooted(col.tree))   {stop("Rooted tree required for phylosor/treeNodf calculation")}
-  one.rows<-treeNodf.one(comm, tree=col.tree, order=order.rows) 
-  resu$treeNODF.rows      <-one.rows$oneNODF
-  resu$treeNODF.rows.pairs<-one.rows$nodf.dist
+  if (is.null(col.tree$edge.length)) {
+     stop("Tree has no branch lengths, cannot compute BL-diversity")
   }
+  
+  if (!is.rooted(col.tree)) {
+     stop("Rooted tree required for treeNodf calculation")
+  }
+  
+  one.rows <- treeNodfOne(comm, tree=col.tree, order=order.rows)
+  resu$rows['tree.nodf']  <- mean(one.rows$tree.nodf.dist)
+  resu$rows['s.fraction'] <- mean(one.rows$s.fraction.dist)
+  resu$rows['topo.nodf']  <- mean(one.rows$tree.nodf.dist - one.rows$s.fraction.dist) 
+  resu$tree.nodf.rows.dist  <- one.rows$tree.nodf.dist
+  resu$s.fraction.rows.dist <- one.rows$s.fraction.dist
+  resu$topo.nodf.rows.dist  <- one.rows$tree.nodf.dist - one.rows$s.fraction.dist
+}
 
 # treeNodf to colums (species)
 if(missing(row.tree)==FALSE){
-  if (is.null(row.tree$edge.length)) {stop("Tree has no branch lengths, cannot compute BL-diversity")}
-  if (!is.rooted(row.tree))   {stop("Rooted tree required for phylosor/treeNodf calculation")}
-  one.cols<-treeNodf.one(t(comm), tree=row.tree, order=order.cols) 
-  resu$treeNODF.cols=one.cols$oneNODF
-  resu$treeNODF.cols.pairs=one.cols$nodf.dist
+  if (is.null(row.tree$edge.length)) {
+     stop("Tree has no branch lengths, cannot compute BL-diversity")
   }
+  
+  if (!is.rooted(row.tree)) {
+     stop("Rooted tree required for treeNodf calculation")
+  }
+  
+  one.cols<-treeNodfOne(t(comm), tree=row.tree, order=order.cols) 
+  resu$cols['tree.nodf']  <- mean(one.cols$tree.nodf.dist)
+  resu$cols['s.fraction'] <- mean(one.cols$s.fraction.dist)
+  resu$cols['topo.nodf']  <- mean(one.cols$tree.nodf.dist - one.cols$s.fraction.dist) 
+  resu$tree.nodf.cols.dist  <- one.cols$tree.nodf.dist
+  resu$s.fraction.cols.dist <- one.cols$s.fraction.dist
+  resu$topo.nodf.cols.dist  <- one.cols$tree.nodf.dist - one.cols$s.fraction.dist
+}
 
 # treeNODF to both margins
 if(missing(col.tree)==FALSE & missing(row.tree)==FALSE) {
-  if (is.null(col.tree$edge.length)) {stop("Tree has no branch lengths, cannot compute BL-diversity")}
-  if (!is.rooted(col.tree)) {stop("Rooted tree required for phylosor/treeNodf calculation")}
-  if (is.null(row.tree$edge.length)) {stop("Tree has no branch lengths, cannot compute BL-diversity")}
-  if (!is.rooted(row.tree)) {stop("Rooted tree required for phylosor/treeNodf calculation")}
-
-  tot<-sum(sum(one.rows$nodf.dist), sum(one.cols$nodf.dist))
-  ndist<-sum(length(one.rows$nodf.dist), length(one.cols$nodf.dist))
-  resu$treeNODF.mat<-tot/ndist 
+  
+  tot.tree       <- sum(sum(one.rows$tree.nodf.dist),  sum(one.cols$tree.nodf.dist))
+  tot.s.fraction <- sum(sum(one.rows$s.fraction.dist), sum(one.cols$s.fraction.dist))
+  ndist <- sum(length(one.rows$tree.nodf.dist), length(one.cols$tree.nodf.dist))
+  resu$mat['tree.nodf']  <- tot.tree       / ndist 
+  resu$mat['s.fraction'] <- tot.s.fraction / ndist
+  resu$mat['topo.nodf']  <- (tot.tree      / ndist ) - (tot.s.fraction / ndist) 
   }
 
-class(resu)<-"treeNodf"
+class(resu) <- "treeNodf"
 resu
 }
 
